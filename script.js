08b7dbecc9d8dceb9b92ff6e6e3e1ac03f359a7f
Carexo <marcel.mietla3@gmail.com>
2021-03-22 18:32:15 +0100

    finish js counter workign with html

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 "use strict";
 
 const form = document.querySelector("form");
+const eventCounter = document.querySelector(".counter-container");
+
+const daysElement = document.querySelector(".days");
+const hoursElement = document.querySelector(".hours");
+const minutesElement = document.querySelector(".minutes");
+const secondsElement = document.querySelector(".seconds");
 
 const inputDay = document.querySelector("#day");
 const inputMonth = document.querySelector("#month");
@@ -19,9 +25,15 @@ function startTimer() {
     const minutes = Math.trunc((differenceTime / 60) % 60);
     const seconds = Math.trunc(differenceTime % 60);
     console.log(differenceTime, days, hours, minutes, seconds);
+    appearEvenCounter(days, hours, minutes, seconds);
 
     if (Math.trunc(differenceTime) === 0) {
       clearInterval(timer);
+      eventCounter.innerHTML = "";
+      eventCounter.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>The event has started</h2>"
+      );
     }
   }
 
@@ -33,7 +45,7 @@ function startTimer() {
   const dateToCount = new Date(valueYear, valueMonth, valueDay, valueHours);
 
   //testing
-  //   const dateToCount = new Date(2021, 2, 22, 17, 23, 5);
+  //   const dateToCount = new Date(2021, 2, 22, 18, 30, 30);
 
   if (
     valueHours &&
@@ -45,9 +57,18 @@ function startTimer() {
     alert("Fill inputs with future date");
   } else if (valueHours && valueDay && valueMonth && valueYear) {
     form.classList.add("hidden");
+    eventCounter.classList.remove("hidden");
     tick();
     timer = setInterval(tick, 1000);
   }
+}
+
+function appearEvenCounter(days, hours, minutes, seconds) {
+  const addPading = (num) => String(num).padStart(2, "0");
+  daysElement.textContent = addPading(days);
+  hoursElement.textContent = addPading(hours);
+  minutesElement.textContent = addPading(minutes);
+  secondsElement.textContent = addPading(seconds);
 }
 
 countButton.addEventListener("click", startTimer);
