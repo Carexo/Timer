75684d3351e7ff1524c8b54ff776a17d5fe579ef
Carexo <marcel.mietla3@gmail.com>
2021-03-22 17:12:30 +0100

    add validation form

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ const inputHours = document.querySelector("#hours");
 
 const countButton = document.querySelector("#submit");
 
-function startTimer(element) {
-  element.preventDefault();
+function startTimer() {
   function tick() {
     const now = new Date();
-    const differenceTime = (DateToCount - now) / 1000;
+    const differenceTime = (dateToCount - now) / 1000;
     const days = Math.trunc(differenceTime / (60 * 60 * 24));
     const hours = Math.trunc((differenceTime / (60 * 60)) % 24);
     const minutes = Math.trunc((differenceTime / 60) % 60);
@@ -23,14 +22,26 @@ function startTimer(element) {
     }
   }
 
+  const valueHours = Number(inputHours.value);
   const valueDay = Number(inputDay.value);
   const valueMonth = Number(inputMonth.value) - 1;
   const valueYear = Number(inputYear.value);
-  const valueHours = Number(inputHours.value);
-  const DateToCount = new Date(valueYear, valueMonth, valueDay, valueHours);
+
+  const dateToCount = new Date(valueYear, valueMonth, valueDay, valueHours);
 
   //testing
-  //   const DateToCount = new Date(2021, 2, 22, 16, 32, 55);
+  //   const dateToCount = new Date(2021, 2, 22, 16, 32, 55);
+
+  if (
+    valueHours &&
+    valueDay &&
+    valueMonth &&
+    valueYear &&
+    dateToCount - new Date() < 0
+  ) {
+    alert("Fill inputs with future date");
+    return 0;
+  }
   tick();
   const timer = setInterval(tick, 1000);
 }
